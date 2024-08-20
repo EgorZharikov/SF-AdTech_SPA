843855c74182e84ce2f1d9bd2d616afa8f02c1f2
”Egor <egorzharikov92@yandex.ru>
2024-08-20 22:56:19 +0700

    update navbar

--- resources/js/components/layouts/Navbar.vue
+++ resources/js/components/layouts/Navbar.vue
@@ -22,33 +22,36 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-
-
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <i class="bi bi-star-fill">Products</i>
-
+                    <li class="nav-item">
+                        <a href="#" class="text-white nav-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="cornflowerblue"
+                                class="bi bi-speedometer" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
+                                <path fill-rule="evenodd"
+                                    d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z" />
+                            </svg>
+                            Dashboard
                         </a>
                     </li>
                     <li class="nav-item">
                         <router-link v-if="!user" class="nav-link" :to="{name: 'login' }">Sign in</router-link>
                     </li>
-
                     <li class="nav-item">
                         <router-link v-if="!user" class="nav-link" :to="{name: 'registration'}">Sign up</router-link>
                     </li>
-
-
                     <li v-if="user" class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="" role="button"
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="" role="button"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="cornflowerblue"
+                                class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                <path fill-rule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                            </svg>
                             {{ user.name }}
                         </a>
-
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <router-link class="dropdown-item" :to="{ name: 'dashboard' }">
-                                Dashboard
-                            </router-link>
                             <a @click.prevent="logout" class="dropdown-item" href="">
                                 Logout
                             </a>
@@ -90,38 +93,3 @@ export default {
 
 }
 </script>
-<style scoped>
- .bd-placeholder-img {
-     font-size: 1.125rem;
-     text-anchor: middle;
-     -webkit-user-select: none;
-     -moz-user-select: none;
-     user-select: none;
- }
-
- @media (min-width: 768px) {
-     .bd-placeholder-img-lg {
-         font-size: 3.5rem;
-     }
- }
-
- .b-example-divider {
-     width: 100%;
-     height: 3rem;
-     background-color: rgba(0, 0, 0, .1);
-     border: solid rgba(0, 0, 0, .15);
-     border-width: 1px 0;
-     box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
- }
-
- .b-example-vr {
-     flex-shrink: 0;
-     width: 1.5rem;
-     height: 100vh;
- }
-
- .bi {
-     vertical-align: -.125em;
-     fill: currentColor;
- }
-</style>
